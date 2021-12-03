5647a77d1e580b737245cb84143b555911aa4c66
corvinus-zobq0t <martin.egyhazi@stud.uni-corvinus.hu>
2021-12-03 22:46:09 +0100

    1st task

---
+++ Mikroszimulacio_week9/Mikroszimulacio_week9/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio_week9.Entities
+{
+    internal class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Kor { get; set; }
+        public double halp { get; set; }
+    }
+}
