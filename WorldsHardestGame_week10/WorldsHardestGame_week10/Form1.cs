1528accb4084599fcc1b86fbbab2c08b04919459
corvinus-zobq0t <martin.egyhazi@stud.uni-corvinus.hu>
2021-12-10 20:50:11 +0100

    4th task

--- WorldsHardestGame_week10/WorldsHardestGame_week10/Form1.cs
+++ WorldsHardestGame_week10/WorldsHardestGame_week10/Form1.cs
@@ -24,12 +24,25 @@ namespace WorldsHardestGame_week10
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+            gc.GameOver += Gc_GameOver;
             for (int i = 0; i <= populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
 
             gc.Start(true);
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
